cd108bffd1a626aa8da26c0bf660e3bc1f87b784
el-davo <david.ahern26@gmail.com>
2018-01-18 21:09:52 +0000

    Initial push

--- src/app/nav/nav.state.ts
+++
@@ -1,9 +0,0 @@
-export const navState: NavState = {
-  showLoginModal: false,
-  showSignupModal: false
-};
-
-export interface NavState {
-  showLoginModal: boolean;
-  showSignupModal: boolean;
-}
