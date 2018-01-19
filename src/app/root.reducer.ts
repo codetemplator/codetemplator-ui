298589e6443ce82db2de75a2fe39880cbaa40aa5
el-davo <david.ahern26@gmail.com>
2018-01-19 20:15:58 +0000

    Ready for deploy

--- src/app/root.reducer.ts
+++ src/app/root.reducer.ts
@@ -1,18 +1,4 @@
 import {combineReducers} from 'redux';
-import {navReducer as nav} from './nav/nav.reducer';
-import {userReducer as user} from './user/user.reducer';
-import {landingReducer as landing} from './landing/landing.reducer';
-import {viewArticleReducer as viewArticle} from './view-article/view-article.reducer';
-import {addArticleReducer as addArticle} from './add-article/add-article.reducer';
-import {editArticleReducer as editArticle} from './edit-article/edit-article.reducer';
-import {sideNavReducer as sideNav} from './side-nav/side-nav.reducer';
 
 export const rootReducer = combineReducers({
-  nav,
-  user,
-  landing,
-  viewArticle,
-  addArticle,
-  editArticle,
-  sideNav
 });
